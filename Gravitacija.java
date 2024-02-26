fec5fbf16451ed1bef3da8f17235a2f9dff3d0ef
Erik Radovicevic <radovicevic.erik1@gmail.com>
2024-02-26 19:26:50 +0100

    Dodana metode gravPospesek

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,20 @@
 public class Gravitacija{
+
+    private static final double G;
+    private static final double M;
+    private static final double r;
+    static {
+        G = 6.674 * Math.pow(10, -11);
+        M = 5.972 * Math.pow(10, 24);
+        r = 6.371 * Math.pow(10, 6);
+    }
+
     public static void main(String[] args){
         System.out.println("OIS je zakon!");
+        //System.out.println(gravPospesek(0));
+    }
+
+    public static double gravPospesek(int v){
+        return (G * M) / Math.pow(r + v, 2);
     }
 }
