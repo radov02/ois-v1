d40a0987cba73a80802f431b9174a1677b2e4c88
Erik Radovicevic <radovicevic.erik1@gmail.com>
2024-02-26 19:53:25 +0100

    dodana funkcija izpisiGravPospesek #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,19 @@
 public class Gravitacija{
+
+    private static final double G;
+    private static final double M;
+    private static final double r;
+    static {
+        G = 6.674 * Math.pow(10, -11);
+        M = 5.972 * Math.pow(10, 24);
+        r = 6.371 * Math.pow(10, 6);
+    }
+
     public static void main(String[] args){
         System.out.println("OIS je zakon!");
+    }
+
+    public static void izpisiGravPospesek(int v){
+        System.out.println((G * M) / Math.pow(r + v, 2));
     }
 }
